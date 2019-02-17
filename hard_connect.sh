db7d46e063d2e5a988166275c4b46261a48664ba
DOS SANTOS <daniel.massy91@gmail.com>
2019-02-17 12:25:48 +0100

    Update hard_connect.sh

--- hard_connect.sh
+++ hard_connect.sh
@@ -14,11 +14,11 @@
 #----------------------------------------------------------------#
 # BASH VERSION: GNU bash 4.4.12
 #================================================================#
-
+clear
 apt-get update && apt-get upgrade -y
-
-nom=$(w | grep tty* | cut -d" " -f1)
-
+clear
+#nom=$(w | grep tty* | cut -d" " -f1)
+nom=$(logname)
 usermod -G disk -a $nom
 
 mkdir /home/$nom/VirtualDisk
@@ -26,9 +26,14 @@ mkdir /home/$nom/VirtualDisk
 cd /home/$nom/VirtualDisk
 
 lsblk
+echo ""
+read -p "Choisir le nom du disque exemple sda, sdb... : " sd
 
-read -p "Choisir le nom du disque exemple sda, sdb..." sd
-
-dat=`date '+%Y-%m-%d %H:%M'`
+dat=$(date '+%H%M')
 
 VBoxManage internalcommands createrawvmdk -filename image$dat.vdi -rawdisk /dev/$sd
+
+chown $nom:$nom image$dat.vdi
+clear
+echo -e "\n Il faut maintenant créer une machine sur virtualBox et ajouter le disque créé \n"
+echo "Fin du script"
