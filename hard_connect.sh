ed64a81522a37d0dec103548b6f607e9f792f009
DOS SANTOS <daniel.massy91@gmail.com>
2023-11-19 19:55:37 +0100

    Update hard_connect.sh

--- hard_connect.sh
+++ hard_connect.sh
@@ -15,29 +15,33 @@ usertos=$(w | awk '{print $1}' | awk 'NR==3')
 chemin=/home/"$usertos"/VirtualDisk/
 sudo apt-get update && apt-get upgrade -y
 clear
+
 sudo usermod -G disk -a "$usertos"
-newgrp disk
+sudo usermod -G vboxuser -a "$usertos"
+#newgrp vboxuser 
+#newgrp disk
+#Il faut reboot !
 
-mkdir "$chemin"
+#mkdir "$chemin"
 
-lsblk
-echo ""
-read -p "Choisir le nom du disque exemple sda, sdb... : " sdsd
+#lsblk
+#echo ""
+#read -p "Choisir le nom du disque exemple sda, sdb... : " sdsd
 
-VBoxManage internalcommands createrawvmdk -filename "$chemin"Disk1.vdi -rawdisk /dev/"$sdsd"
+#VBoxManage internalcommands createrawvmdk -filename "$chemin"Disk1.vdi -rawdisk /dev/"$sdsd"
 
-clear
+#clear
 
 # détection du port utilisé par le contrôleur
 # sur quel contrôleur agir
-vboxmanage showvminfo Debian | grep "Storage Controller Name"
+#vboxmanage showvminfo Debian | grep "Storage Controller Name"
 
 # sur quel port agir
 
 
-vboxmanage list vms
-echo ""
-read -p "Sur quelle vm attacher ce nouveau disque ? " nomVM
-#vboxmanage storagectl "$nomVM" --name "SATA Controller" --add sata --controller IntelAHCI --portcount 1 --bootable on
-vboxmanage storageattach "$nomVM" --storagectl "SATA" --device 0 --port 2 --type hdd --medium "$chemin"Disk1.vdi
-echo "Fin du script"
+#vboxmanage list vms
+#echo ""
+#read -p "Sur quelle vm attacher ce nouveau disque ? " nomVM
+######################vboxmanage storagectl "$nomVM" --name "SATA Controller" --add sata --controller IntelAHCI --portcount 1 --bootable on
+#vboxmanage storageattach "$nomVM" --storagectl "SATA" --device 0 --port 2 --type hdd --medium "$chemin"Disk1.vdi
+#echo "Fin du script"
